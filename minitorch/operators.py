1c3e62d0d94257375c4675ccb70725fe4b128406
Daniel Vera Nieto <daniel.vera-nieto@psi.ch>
2025-01-31 14:00:09 +0100

    Finished first assignment

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -1,9 +1,9 @@
 """Collection of the core mathematical operators used throughout the code base."""
 
 import math
+from typing import Callable, Iterable
 
 # ## Task 0.1
-from typing import Callable, Iterable
 
 #
 # Implementation of a prelude of elementary functions.
@@ -35,6 +35,256 @@ from typing import Callable, Iterable
 # TODO: Implement for Task 0.1.
 
 
+def mul(a: float, b: float) -> float:
+    """Multiplies two numbers
+
+    Args:
+    ----
+        a (float): a number
+        b (float): another number
+
+    Returns:
+    -------
+        float: the product of the two numbers
+
+    """
+    return a * b
+
+
+def id(a: float) -> float:
+    """_summary_
+
+    Args:
+    ----
+        a (float): _description_
+
+    Returns:
+    -------
+        float: _description_
+
+    """
+    return a
+
+
+def add(a: float, b: float) -> float:
+    """_summary_
+
+    Args:
+    ----
+        a (float): _description_
+        b (float): _description_
+
+    Returns:
+    -------
+        float: _description_
+
+    """
+    return a + b
+
+
+def neg(a: float) -> float:
+    """_summary_
+
+    Args:
+    ----
+        a (float): _description_
+
+    Returns:
+    -------
+        float: _description_
+
+    """
+    return -a
+
+
+def lt(a: float, b: float) -> bool:
+    """A < b
+
+    Args:
+    ----
+        a (float): _description_
+        b (float): _description_
+
+    Returns:
+    -------
+        bool: _description_
+
+    """
+    return a < b
+
+
+def eq(a: float, b: float) -> bool:
+    """A == b
+
+    Args:
+    ----
+        a (float): _description_
+        b (float): _description_
+
+    Returns:
+    -------
+        bool: _description_
+
+    """
+    return a == b
+
+
+def max(a: float, b: float) -> float:
+    """_summary_
+
+    Args:
+    ----
+        a (float): _description_
+        b (float): _description_
+
+    Returns:
+    -------
+        float: _description_
+
+    """
+    return a if lt(b, a) else b
+
+
+def is_close(a: float, b: float, tol: float = 1e-2) -> float:
+    """$f(x) = |x - y| < 1e-2$
+
+    Args:
+    ----
+        a (float): _description_
+        b (float): _description_
+        tol (float, optional): _description_. Defaults to 1e-4.
+
+    Returns:
+    -------
+        float: _description_
+
+    """
+    return abs(a - b) < tol
+
+
+def sigmoid(x: float) -> float:
+    r"""$f(x) =  \frac{1.0}{(1.0 + e^{-x})}$ if x >=0 else $\frac{e^x}{(1.0 + e^{x})}$
+
+    Args:
+    ----
+        x (float): _description_
+
+    Returns:
+    -------
+        float: _description_
+
+    """
+    return 1 / (1 + exp(-x)) if x >= 0 else exp(x) / (1 + exp(x))
+
+
+def relu(x: float) -> float:
+    """_summary_
+
+    Args:
+    ----
+        x (float): _description_
+
+    Returns:
+    -------
+        float: _description_
+
+    """
+    return x if x >= 0 else 0
+
+
+def log(x: float) -> float:
+    """_summary_
+
+    Args:
+    ----
+        x (float): _description_
+
+    Returns:
+    -------
+        float: _description_
+
+    """
+    return math.log(x)
+
+
+def exp(x: float) -> float:
+    """_summary_
+
+    Args:
+    ----
+        x (float): _description_
+
+    Returns:
+    -------
+        float: _description_
+
+    """
+    return math.e**x
+
+
+def inv(x: float) -> float:
+    """Calculates the reciprocal
+
+    Args:
+    ----
+        x (float): _description_
+
+    Returns:
+    -------
+        float: _description_
+
+    """
+    return 1 / x
+
+
+def log_back(x: float, y: float) -> float:
+    """Computes the derivative of log times a second arg
+
+    Args:
+    ----
+        x (float): _description_
+        y (float): _description_
+
+    Returns:
+    -------
+        float: _description_
+
+    """
+    return 1 / x * y
+
+
+def inv_back(x: float, y: float) -> float:
+    """Computes the derivative of reciprocal times a second arg
+
+    Args:
+    ----
+        x (float): _description_
+        y (float): _description_
+
+    Returns:
+    -------
+        float: _description_
+
+    """
+    return -1 / x**2 * y
+
+
+def relu_back(x: float, y: float) -> float:
+    """Computes the derivative of ReLU times a second arg
+
+    Args:
+    ----
+        x (float): _description_
+        y (float): _description_
+
+    Returns:
+    -------
+        float: _description_
+
+    """
+    return y if x >= 0 else 0
+
+
 # ## Task 0.3
 
 # Small practice library of elementary higher-order functions.
@@ -52,3 +302,128 @@ from typing import Callable, Iterable
 
 
 # TODO: Implement for Task 0.3.
+
+
+def map(fn: Callable[[float], float]) -> Callable[[Iterable[float]], Iterable[float]]:
+    """Higher-order function that applies a given function to each element of an iterable
+
+    Args:
+    ----
+        fn (Callable[[float], float]): _description_
+
+    Returns:
+    -------
+        Callable[[Iterable[float]], Iterable[float]]: _description_
+
+    """
+
+    def apply(ls: Iterable[float]) -> Iterable[float]:
+        return [fn(x) for x in ls]
+
+    return apply
+
+
+def zipWith(
+    fn: Callable[[float, float], float],
+) -> Callable[[Iterable[float], Iterable[float]], Iterable[float]]:
+    """Higher-order function that combines elements from two iterables using a given function
+
+    Args:
+    ----
+        fn (Callable[[float,float], float]): _description_
+
+    Returns:
+    -------
+        Callable[[Iterable[float], Iterable[float]], Iterable[float]]: _description_
+
+    """
+
+    def apply(l1: Iterable[float], l2: Iterable[float]) -> Iterable[float]:
+        return [fn(el1, l2[idx]) for idx, el1 in enumerate(l1)]
+
+    return apply
+
+
+def reduce(fn: Callable[[float, float], float]) -> Callable[[Iterable[float]], float]:
+    """Higher-order function that reduces an iterable to a single value using a given function
+
+    Args:
+    ----
+        fn (Callable[[float], float]): _description_
+
+    Returns:
+    -------
+        Callable[[Iterable[float]], float]: _description_
+
+    """
+
+    def apply(ls: Iterable[float]) -> float:
+        if not ls:
+            return 0  # a bit adhoc IMO
+        res = ls[0]
+        for el in ls[1:]:
+            res = fn(res, el)
+        return res
+
+    return apply
+
+
+def negList(ls: Iterable[float]) -> Iterable[float]:
+    """Negate all elements in a list using map
+
+    Args:
+    ----
+        ls (Iterable[float]): _description_
+
+    Returns:
+    -------
+        Iterable[float]: _description_
+
+    """
+    return map(neg)(ls)
+
+
+def addLists(ls1: Iterable[float], ls2: Iterable[float]) -> Iterable[float]:
+    """Add corresponding elements from two lists using zipWith
+
+    Args:
+    ----
+        ls1 (Iterable[float]): _description_
+        ls2 (Iterable[float]): _description_
+
+    Returns:
+    -------
+        Iterable[float]: _description_
+
+    """
+    return zipWith(add)(ls1, ls2)
+
+
+def sum(ls: Iterable[float]) -> float:
+    """Sum all elements in a list using reduce
+
+    Args:
+    ----
+        ls (Iterable[float]): _description_
+
+    Returns:
+    -------
+        float: _description_
+
+    """
+    return reduce(add)(ls)
+
+
+def prod(ls: Iterable[float]) -> float:
+    """Calculate the product of all elements in a list using reduce
+
+    Args:
+    ----
+        ls (Iterable[float]): _description_
+
+    Returns:
+    -------
+        float: _description_
+
+    """
+    return reduce(mul)(ls)
--- tests/test_operators.py
+++ tests/test_operators.py
@@ -23,6 +23,8 @@ from minitorch.operators import (
     relu,
     relu_back,
     sigmoid,
+    log,
+    exp,
 )
 
 from .strategies import assert_close, small_floats
@@ -108,7 +110,12 @@ def test_sigmoid(a: float) -> None:
     * It is  strictly increasing.
     """
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert (lt(0, sigmoid(a)) or eq(sigmoid(a), 1)) and (
+        lt(sigmoid(a), 1) or eq(sigmoid(a), 1)
+    )
+    assert_close(add(1, neg(sigmoid(a))), sigmoid(neg(a)))
+    assert_close(sigmoid(0), 0.5)
+    assert lt(sigmoid(a), sigmoid(a + 1)) or eq(sigmoid(a), 1)
 
 
 @pytest.mark.task0_2
@@ -116,32 +123,36 @@ def test_sigmoid(a: float) -> None:
 def test_transitive(a: float, b: float, c: float) -> None:
     """Test the transitive property of less-than (a < b and b < c implies a < c)"""
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    if lt(a, b) and lt(b, c):
+        assert lt(a, c)
 
 
 @pytest.mark.task0_2
-def test_symmetric() -> None:
+@given(small_floats, small_floats)
+def test_symmetric(a: float, b: float) -> None:
     """Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert eq(mul(a, b), mul(b, a))
 
 
 @pytest.mark.task0_2
-def test_distribute() -> None:
+@given(small_floats, small_floats, small_floats)
+def test_distribute(x: float, y: float, z: float) -> None:
     r"""Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(mul(z, add(x, y)), add(mul(z, x), mul(z, y)))
 
 
 @pytest.mark.task0_2
-def test_other() -> None:
+@given(small_floats)
+def test_other(x: float) -> None:
     """Write a test that ensures some other property holds for your functions."""
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(log(exp(x)), x)
 
 
 # ## Task 0.3  - Higher-order functions
@@ -169,7 +180,10 @@ def test_sum_distribute(ls1: List[float], ls2: List[float]) -> None:
     is the same as the sum of each element of `ls1` plus each element of `ls2`.
     """
     # TODO: Implement for Task 0.3.
-    raise NotImplementedError("Need to implement for Task 0.3")
+    assert_close(
+        add(minitorch.operators.sum(ls1), minitorch.operators.sum(ls2)),
+        minitorch.operators.sum(addLists(ls1, ls2)),
+    )
 
 
 @pytest.mark.task0_3
